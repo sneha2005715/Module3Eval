7690f3b0c73c8cf4b11d1abc817739bbb6130d7b
snehanjali2005 <snehanjalirout2005@gmail.com>
2026-01-03 19:15:51 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,27 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (!email || !password) return alert("Fill all fields");
+    const path = login(email, password);
+    if (path) navigate(path);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
